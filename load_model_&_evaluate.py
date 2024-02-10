7cd34be99d9916967aa10af70ce8c016eb04652e
Felipe Cockles <113461788+cockles98@users.noreply.github.com>
2024-02-09 21:39:50 -0300

    Update load_model_&_evaluate.py

--- load_model_&_evaluate.py
+++ load_model_&_evaluate.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import pandas as pd
 
 # Load the model
-loaded_model = tf.keras.models.load_model('D:\\GitHub Dic\\breast_cancer_prediction\\saved_model')
+loaded_model = tf.keras.models.load_model('D:\\GitHub Dic\\breast_cancer_prediction\\saved_model')    # Make sure you put the correct folder path
 
 # Print model summary
 loaded_model.summary()
@@ -11,8 +11,8 @@ loaded_model.summary()
 loaded_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 # Load test data
-features = pd.read_csv('D:\\GitHub Dic\\breast_cancer_prediction\\dataset\\labels_dataset.csv')
-classes = pd.read_csv('D:\\GitHub Dic\\breast_cancer_prediction\\dataset\\target_dataset.csv')
+features = pd.read_csv('D:\\GitHub Dic\\breast_cancer_prediction\\dataset\\labels_dataset.csv')       # make sure you put the correct file path
+classes = pd.read_csv('D:\\GitHub Dic\\breast_cancer_prediction\\dataset\\target_dataset.csv')  
 
 # Evaluate the model
 loss, accuracy = loaded_model.evaluate(features, classes)
